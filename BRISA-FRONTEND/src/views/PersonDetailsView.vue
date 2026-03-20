1a7ed6b6239f167a5cb7539e44cd0cb5c8d1a781
Athisson Alberto <athissonlm17@gmail.com>
2026-03-19 22:52:22 -0300

    .

--- BRISA-FRONTEND/src/views/PersonDetailsView.vue
+++ BRISA-FRONTEND/src/views/PersonDetailsView.vue
@@ -134,7 +134,7 @@
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                   <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
                 </svg>
-                Nível de Escolaridade
+                Escolaridade
               </label>
               <input 
                 v-model="editForm.educationLevel" 
@@ -151,7 +151,7 @@
                   <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
                   <circle cx="12" cy="10" r="3"></circle>
                 </svg>
-                Endereço
+                CEP
               </label>
               <input 
                 v-model="editForm.address" 
@@ -168,7 +168,7 @@
                   <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7z"></path>
                   <circle cx="12" cy="9" r="2.5"></circle>
                 </svg>
-                Cidade
+                Cidade - UF
               </label>
               <input 
                 v-model="editForm.city" 
